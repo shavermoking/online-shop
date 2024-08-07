8cf4b4f65331c733805711d5814dc7861fd0a9fb
Никита Чуркин <shavermoking@gmail.com>
2024-08-07 21:52:44 +0300

    Revert "working on testing"
    This reverts commit 00c8872083b5db3f9a2294786e2c662428d30278.

--- config/database.php
+++ config/database.php
@@ -46,30 +46,21 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'mysql'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'sail'),
+            'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-        ],
-
-        'test-mysql' => [
-            'driver' => 'mysql',
-            'host' => env('TEST_DB_HOST', 'mysql-test'),
-            'port' => env('TEST_DB_PORT', '3306'),
-            'database' => env('TEST_DB_DATABASE', 'test-laravel'),
-            'username' => env('TEST_DB_USERNAME', 'sail'),
-            'password' => env('TEST_DB_PASSWORD', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'pgsql' => [

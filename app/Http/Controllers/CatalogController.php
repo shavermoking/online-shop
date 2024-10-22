9c40182f08f5a6c004e5ce7414b100329f70c6a8
Никита Чуркин <shavermoking@gmail.com>
2024-10-22 07:43:52 +0300

    new filter logic. Added BrandFilter and Price Filter. stage-5-lesson-4 is over

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -14,11 +14,6 @@ class CatalogController extends Controller
 {
     public function __invoke(?Category $category): Factory|View|Application
     {
-        $brands = Brand::query()
-            ->select(['id', 'title'])
-            ->has('products')
-            ->get();
-
         $categories = Category::query()
             ->select(['id', 'title', 'slug'])
             ->has('products')
@@ -44,7 +39,6 @@ class CatalogController extends Controller
         return view('catalog.index', [
             'products' => $products,
             'categories' => $categories,
-            'brands' => $brands,
             'category' => $category
         ]);
     }
